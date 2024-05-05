f2297a80c06fd8e95a08f57469a3d94119fa6330
Gabriel Andrade <joaogabriel2@gmail.com>
2024-05-05 17:50:21 -0300

    wip: creating room details screen

---
+++ addons/roguelike_system/RogueSys.gd
@@ -0,0 +1,33 @@
+@tool
+extends EditorPlugin
+
+const MainPanel = preload("res://addons/roguelike_system/nodes/main_panel.tscn")
+
+var main_panel_instance
+
+func _enter_tree():
+	main_panel_instance = MainPanel.instantiate()
+	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	_make_visible(false)
+
+
+func _exit_tree():
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name():
+	return "Roguelike System"
+
+
+func _get_plugin_icon():
+	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
