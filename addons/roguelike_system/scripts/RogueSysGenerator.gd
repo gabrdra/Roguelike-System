ac12bfdf14325e94c4005bb994487771c8d0395f
Gabriel Andrade <joaogabriel2@gmail.com>
2024-09-26 10:47:55 -0300

    feat: Transformed SimpleQueue into SimpleDeque

--- addons/roguelike_system/scripts/RogueSysGenerator.gd
+++ addons/roguelike_system/scripts/RogueSysGenerator.gd
@@ -15,61 +15,67 @@ func _create_used_room(room:Room)->Room:
 func _choose_random_element_array(array:Array):
 	return array[random.randi_range(0,array.size()-1)]
 
-func _get_unused_connections_from_room(room:Room) -> SimpleQueue:
+func _get_unused_connections_from_room(room:Room) -> SimpleDeque:
 	var passages_names:Array[String]
 	for passage_name in room.passages:
 		if room.passages[passage_name]==null:
 			passages_names.append(passage_name)
-	var queue = SimpleQueue.new()
+	var deque = SimpleDeque.new()
 	while not passages_names.is_empty():
 		var index = random.randi_range(0,passages_names.size()-1)
 		var passage_name := passages_names[index]
 		passages_names.remove_at(index)
-		queue.insert(Connection.new(room, passage_name))
-	return queue
+		deque.insert_back(Connection.new(room, passage_name))
+	return deque
 	
-func generate_level(input_level:LevelData, input_seed:int = 0, attempts:int = 1000000) -> LevelData:
+func generate_level(input_level:LevelData, input_seed:int = 0) -> LevelData:
 	random = RandomNumberGenerator.new()
-	for i in range(attempts):
-		var input_rooms:Dictionary = input_level.rooms
-		var used_rooms:= {}
-		used_rooms[input_level.starter_room.name] = _create_used_room(input_level.starter_room)
-		var queue = _get_unused_connections_from_room(used_rooms[input_level.starter_room.name])
-		var successful := true
-		while successful:
-			if queue.is_empty():
-				break
-			var outgoing_connection:Connection = queue.pop_front()
-			if outgoing_connection.room.passages[outgoing_connection.connected_passage]!=null:
-				#this connection was already used
-				continue
-			var possible_connections:Array[Connection] = input_rooms[outgoing_connection.room.name].passages[outgoing_connection.connected_passage].filter(
-				func (c:Connection):
-					#a connection is only possible the other side is not already being used
-					if used_rooms.has(c.room.name):
-						return used_rooms[c.room.name].passages[c.connected_passage] == null
-					return true
-			)
-			if possible_connections.size() == 0:
-				#there was no correspondent to the outgoing_connection, 
-				#meaning a invalid state was created aborts generation and tries again
-				successful = false
-				break
-			var incomming_connection:Connection = _choose_random_element_array(possible_connections)
-			if not used_rooms.has(incomming_connection.room.name):
-				#if a room isn't on used_rooms yet it should be inserted
-				# and it's passages added to the queue
-				var new_used_room:Room = _create_used_room(incomming_connection.room)
-				used_rooms[new_used_room.name] = new_used_room
-				new_used_room.passages[incomming_connection.connected_passage] = outgoing_connection
-				outgoing_connection.room.passages[outgoing_connection.connected_passage] = Connection.new(new_used_room, incomming_connection.connected_passage)
-				queue.insert_queue(_get_unused_connections_from_room(new_used_room))
-			else:
-				incomming_connection.room.passages[incomming_connection.connected_passage] = outgoing_connection
-				outgoing_connection.room.passages[outgoing_connection.connected_passage] = incomming_connection
-		if successful:
-			var return_level = LevelData.new()
-			return_level.rooms = used_rooms
-			return_level.starter_room = used_rooms[input_level.starter_room.name]
-			return return_level
+	var input_rooms:Dictionary = input_level.rooms
+	var used_rooms:= {}
+	used_rooms[input_level.starter_room.name] = _create_used_room(input_level.starter_room)
+	var deque = _get_unused_connections_from_room(used_rooms[input_level.starter_room.name])
+	var successful := true
+	while successful:
+		if deque.is_empty():
+			break
+		var outgoing_connection:Connection = deque.pop_front()
+		if outgoing_connection.room.passages[outgoing_connection.connected_passage]!=null:
+			#this connection was already used
+			continue
+		var possible_connections:Array[Connection] = input_rooms[outgoing_connection.room.name].passages[outgoing_connection.connected_passage].filter(
+			func (c:Connection):
+				#a connection is only possible the other side is not already being used
+				if used_rooms.has(c.room.name):
+					return used_rooms[c.room.name].passages[c.connected_passage] == null
+				return true
+		)
+		if possible_connections.size() == 0:
+			#there was no correspondent to the outgoing_connection, 
+			#meaning a invalid state was created aborts generation and tries again
+			successful = false
+			break
+		var incomming_connection:Connection = _choose_random_element_array(possible_connections)
+		if not used_rooms.has(incomming_connection.room.name):
+			#if a room isn't on used_rooms yet it should be inserted
+			# and it's passages added to the deque
+			var new_used_room:Room = _create_used_room(incomming_connection.room)
+			used_rooms[new_used_room.name] = new_used_room
+			new_used_room.passages[incomming_connection.connected_passage] = outgoing_connection
+			outgoing_connection.room.passages[outgoing_connection.connected_passage] = Connection.new(new_used_room, incomming_connection.connected_passage)
+			deque.insert_deque_back(_get_unused_connections_from_room(new_used_room))
+		else:
+			incomming_connection.room.passages[incomming_connection.connected_passage] = outgoing_connection
+			outgoing_connection.room.passages[outgoing_connection.connected_passage] = incomming_connection
+	if successful:
+		var return_level = LevelData.new()
+		return_level.rooms = used_rooms
+		return_level.starter_room = used_rooms[input_level.starter_room.name]
+		return return_level
 	return LevelData.new()
+
+
+class ErrorCorrection:
+	var parent_room_name:String #room that inserted this room into the final map
+	var room:Room
+	var passages_attempts:Dictionary
+	var deque_copy:SimpleDeque
